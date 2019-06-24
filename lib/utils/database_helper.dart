51516b92949d49bd3b5a65e7ca16de7db9b0cb7b
CallieAnn <dedorec@my.lanecc.edu>
2019-06-24 15:59:03 -0700

    database helper class

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,90 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_notekeeper/models/note.dart';
+
+class DatabaseHelper{
+  static DatabaseHelper _databaseHelper; //singleton dbhelper
+  static Database _database;             //singleton db
+
+  String noteTable = 'note_table';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String colPriority = 'priority';
+  String colDate = 'date';
+
+  DatabaseHelper._createInstance();//Named constructor to create instance of DatabaseHelper
+
+  factory DatabaseHelper() {
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper._createInstance();// This is executed only once, singleton object
+    }
+    return _databaseHelper;
+  }
+
+  Future<Database> get database async{
+
+    if (_database == null){
+      _database = await initializeDatabase();
+    }
+    return _database;
+  }
+
+  Future<Database> initializeDatabase() async {
+    //Get the directory path for both Android and IOS to store database
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'notes.db';
+
+    //open/create the database at a given path
+    var notesDatabase = await openDatabase(path, version: 1, onCreate: _createDb);
+
+    return notesDatabase;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
+    '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)');
+  }
+
+  //Fetch: get all note objects from db
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+
+    var result = await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    return result;
+  }
+
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  Future<int> updateNote(Note note) async {
+    var db = await this.database;
+    var result = await db.update(noteTable, note.toMap(), where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> deleteNote(int id) async {
+    var db = await this.database;
+    int result = await db.rawDelete('DELETE FROM $noteTable WHERE $colId = $id');
+    return result;
+  }
+
+  //get number of records in table
+  Future<int> getCount() async {
+    Database db = await this.database;
+    List<Map<String, dynamic>> x = await db.rawQuery('SELECT COUNT (*) from $noteTable');
+    int result = Sqflite.firstIntValue(x);
+    return result;
+
+  }
+
+
+
+
+
+}
